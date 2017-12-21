ea26176cf85f753c00286576f2b5b25cd84f9ed3
Trần Tiến Hiệp <tienhieptran@outlook.com>
2017-12-21 22:50:40 +0700

    HomePage Done !!!

--- SoilCare/SoilCare.Android/MainActivity.cs
+++ SoilCare/SoilCare.Android/MainActivity.cs
@@ -1,19 +1,90 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using System;
+using SoilCare.Android.Fragments;
 
 namespace SoilCare.Android
 {
-    [Activity(Label = "SoilCare.Android", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.DarkActionBar")]
+    public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        BottomNavigationView bottomNavigation;
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+            base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
+            //var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            //if (toolbar != null)
+            //{
+            //    SetSupportActionBar(toolbar);
+            //    SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            //    SupportActionBar.SetHomeButtonEnabled(false);
+
+            //}
+
+            bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+
+            bottomNavigation.SetShiftMode(false, false);
+
+            bottomNavigation.NavigationItemSelected += BottomNavigation_NavigationItemSelected;
+
+            LoadFragment(Resource.Id.menu_home);
         }
+
+        private void BottomNavigation_NavigationItemSelected(object sender, BottomNavigationView.NavigationItemSelectedEventArgs e)
+        {
+            LoadFragment(e.Item.ItemId);
+        }
+
+        private void LoadFragment(int id)
+        {
+            Fragment fragment = null;   
+            switch (id)
+            {
+                case Resource.Id.menu_home:
+                    fragment = new HomeFragment();
+                    ReplaceFragment(fragment);
+                    break;
+                case Resource.Id.menu_library:
+                    fragment = new LibraryFragment();
+                    ReplaceFragment(fragment);
+                    break;
+                case Resource.Id.menu_account:
+                    fragment = new AccountFragment();
+                    ReplaceFragment(fragment);
+                    break;
+                case Resource.Id.menu_settings:
+                    fragment = new SettingsFragment();
+                    ReplaceFragment(fragment);
+                    break;
+            }
+            if (fragment == null)
+                return;
+
+          
+
+        }
+
+        private void ReplaceFragment(Fragment fragment)
+        {
+            // Create a new fragment and a transaction.
+            FragmentTransaction fragmentTx = this.FragmentManager.BeginTransaction();
+
+            // Replace the fragment that is in the View fragment_container (if applicable).
+            fragmentTx.Replace(Resource.Id.content_frame, fragment);
+
+            // Add the transaction to the back stack.
+            fragmentTx.AddToBackStack(null);
+
+            // Commit the transaction.
+            fragmentTx.Commit();
+        }
+
+
     }
 }
 
