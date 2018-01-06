708ec0eff50a0462f483a4e01bb4914c8deb3569
Trần Tiến Hiệp <tienhieptran@outlook.com>
2018-01-06 16:50:27 +0700

    6/1/2018

---
+++ SoilCare/SoilCare.Android/UserLandActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace SoilCare.Android
+{
+    [Activity(Label = "UserLandActivity", Theme = "@style/CustomActionBarTheme")]
+    public class UserLandActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.UserLand);
+            // Create your application here
+        }
+    }
+}
